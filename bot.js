8cd7e239cba3790651ac53d1b4132f78c2be4024
turbobots11 <53749152+turbobots11@users.noreply.github.com>
2019-08-08 19:15:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ m.sendMessage(args)
    }
    });
 
-const developers = ["472413769700474901","id"]
+const developers = ["559402149285724210"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
